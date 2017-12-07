b5709ab79ab83b3d750dd2d77226465bfb4a3b45
VinSevilla <vinsevilla24@gmail.com>
2017-12-07 08:03:53 -0800

    Update AdventureBegin.java

--- AdventureBegin.java
+++ AdventureBegin.java
@@ -1,10 +1,4 @@
 
-/**
- * Write a description of class AdventureBegin here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class AdventureBegin
 {
     public static void adventureBegin()
