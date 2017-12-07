905a81e080e9bc2d86311e018f8a9145e385a098
VinSevilla <vinsevilla24@gmail.com>
2017-12-07 08:01:44 -0800

    Update AdventureBegin.java

--- AdventureBegin.java
+++ AdventureBegin.java
@@ -9,23 +9,16 @@ public class AdventureBegin
 {
     public static void adventureBegin()
     {
-        System.out.println("You are an adventurer in search for mythic treassure. That search has brought you here");
-        System.out.println("You find yourself in the first room of a castle that is said to hold the tressure");
-        System.out.println("There's a key on a pedestal in between two doors, each leading to diffrent wings of the mannor");
-        System.out.println("The door behind you slams shut with a lock. ");
-        System.out.println("Suddenly, a small fire starts in the room and its growing, fast.");
-        System.out.println("Somebody wants you dead.");
-        System.out.println("What do you do?");
-        System.out.println("");
-        System.out.println("1.) Try to put out the fire before it grows");
-        System.out.println("2.) Take the key from the pedestal");
-        System.out.println("3.) Try the door on the left");
-        System.out.println("4.) Try the door on the right");
-        System.out.println("5.) Try to break a window out of the room");
-        System.out.println("6.) Look for clues about who wants you dead");
-        System.out.println("7.) Pray to your god");
-        System.out.println("8.) Use the fire to melt the key, then collect the goop. Use goop on door, to create magic portal");
-        System.out.println("9.) Die");
+        System.out.println("Congradulations! you're a successful Highschool basketball prospect being scouted by multiple colleges");
+        System.out.println("Many respected univiersities would love to have you represent them");
+        System.out.println("Whose application would you like to fill out?");
+        
+        System.out.println("[1] Kentucky University");
+        System.out.println("[2] Duke University");
+        System.out.println("[3] UCLA");
+        System.out.println("[4] University of North Carolina");
+        System.out.println("[5] University of Kansas");
+        
 
     }
 }
